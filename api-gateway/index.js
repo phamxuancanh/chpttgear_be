e22da718f5975ef129b6b503c34827e244110d62
phamxuancanh <pxcpaze@gmail.com>
2025-01-11 01:18:13 +0700

    config gateway

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = 6868;
 
-// Import và sử dụng middleware
 require('./middlewares/proxy')(app);
 
 app.get('/', (req, res) => {
