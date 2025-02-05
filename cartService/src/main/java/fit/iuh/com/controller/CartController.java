56a248da655f8c5036231b0f9a4a0e2634e0f1bb
hy.phog_ <phanhuyphong20@gmail.com>
2025-02-05 19:42:16 +0700

    edit api

--- cartService/src/main/java/fit/iuh/com/controller/CartController.java
+++ cartService/src/main/java/fit/iuh/com/controller/CartController.java
@@ -2,7 +2,6 @@ package fit.iuh.com.controller;
 
 import fit.iuh.com.model.Cart;
 import fit.iuh.com.model.CartItem;
-import fit.iuh.com.service.CartItemService;
 import fit.iuh.com.service.CartService;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,32 +12,18 @@ import java.util.UUID;
 @RequestMapping("/api/v1")
 public class CartController {
     private final CartService cartService;
-    private final CartItemService cartItemService;
 
-    public CartController(CartService cartService, CartItemService cartItemService) {
+    public CartController(CartService cartService) {
         this.cartService = cartService;
-        this.cartItemService = cartItemService;
     }
-
-    /**
-     * GET
-     */
-    // GET ALL
 
     @GetMapping("/carts")
     public List<Cart> getAllCart () {
         return cartService.getAll();
     }
 
-    @GetMapping("/cart_items")
-    public List<CartItem> getAllCartItem() {
-        return cartItemService.getAll();
-    }
-
-    // GET BY ID
-
     @GetMapping("/carts/{id}")
-    public Cart getCart (@PathVariable UUID id) {
+    public Cart getCartById (@PathVariable UUID id) {
         Cart cart = cartService.getById(id);
         if (cart == null) {
             return null;
@@ -47,50 +32,18 @@ public class CartController {
         }
     }
 
-    @GetMapping("/cart_items/{id}")
-    public CartItem getCartItemById(@PathVariable UUID id) {
-        CartItem cartItem = cartItemService.getById(id);
-        if (cartItem == null) {
-            return null;
-        } else {
-            return cartItem;
-        }
-    }
-
-    /**
-     * POST
-     */
-    // CREATE
-
     @PostMapping("/carts")
     public Cart createCart(@RequestBody Cart cart) {
         return cartService.createOne(cart);
     }
 
-    @PostMapping("/cart_items")
-    public CartItem createCartItem(@RequestBody CartItem cartItem) {
-        return cartItemService.createOne(cartItem);
-    }
-
-    // UPDATE
     @PutMapping("/carts/{id}")
     public Cart updateCart(@RequestBody Cart cart, @PathVariable UUID id) {
         return cartService.updateOne(cart, id);
     }
 
-    @PutMapping("/cart_items/{id}")
-    public CartItem updateCartItem(@RequestBody CartItem cartItem, @PathVariable UUID id) {
-        return cartItemService.updateOne(cartItem, id);
-    }
-
-    // DELETE
     @DeleteMapping("/carts/{id}")
     public Cart deleteCart(@PathVariable UUID id) {
         return cartService.deleteOne(id);
-    }
-
-    @DeleteMapping("/cart_items/{id}")
-    public CartItem deleteCartItem(@PathVariable UUID id) {
-        return cartItemService.deleteOne(id);
     }
 }
--- ratingService/src/main/java/com/example/ratingservice/controller/Product_Rating_Controller.java
+++ ratingService/src/main/java/com/example/ratingservice/controller/Product_Rating_Controller.java
@@ -4,6 +4,7 @@ import com.example.ratingservice.service.Product_Rating_Service;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -17,6 +18,26 @@ public class Product_Rating_Controller {
 
     @GetMapping("/product_ratings")
     public List<Product_Rating> getAllProductRatings(){
-        return productRatingService.getAllProductRating();
+        return productRatingService.getAll();
+    }
+
+    @GetMapping("/product_ratings/{id}")
+    public Product_Rating getProductRating(@PathVariable UUID id){
+        return productRatingService.getById(id);
+    }
+
+    @PostMapping("/product_ratings")
+    public Product_Rating createProductRating(@RequestBody Product_Rating productRating){
+        return productRatingService.createOne(productRating);
+    }
+
+    @PutMapping("/product_ratings/{id}")
+    public Product_Rating updateProductRating(@RequestBody Product_Rating productRating, @PathVariable UUID id){
+        return productRatingService.updateOne(productRating, id);
+    }
+
+    @DeleteMapping("/product_ratings{id}")
+    public Product_Rating deleteProductRating(@PathVariable UUID id){
+        return productRatingService.deleteOne(id);
     }
 }
