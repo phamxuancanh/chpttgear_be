de297be7397534521bb7eb80afc81264e5d19f05
Bảo Trâm <baotrammari2003@gmail.com>
2025-02-28 07:47:34 +0700

    fix update

--- cartService/src/main/resources/data/cart.sql
+++ cartService/src/main/resources/data/cart.sql
@@ -3,12 +3,6 @@ VALUES
 	('2de39983-9975-4e84-ba34-6296f5cef64a', 'c889be20-5741-447d-9e66-09c8fedc8bcd'), --Tram
 	('5c30420c-b07c-4264-915f-6d3bf51e6243', '0e7d3141-a864-4b1a-8835-85e5b999672a'); --Thu
 
-SELECT cart_id, user_id
-	FROM public.carts;
-
-SELECT cart_item_id, product_id, quantity, cart_id
-	FROM public.cart_items;
-
 INSERT INTO public.cart_items
 	(cart_item_id, product_id, quantity, cart_id)
 VALUES 
@@ -17,4 +11,13 @@ VALUES
 	('b3402cad-567a-4bb6-92ba-0e17f1aae6f4', '6d93a38a-7efb-4b74-a32f-6681dbdafff3', '2de39983-9975-4e84-ba34-6296f5cef64a', 3),
 	('1efc1a4e-54cb-491d-bfb6-7ebddeaf3b3b', '74f99a6d-75fd-4c5b-a3e8-8f5e7a9c2b1d', '2de39983-9975-4e84-ba34-6296f5cef64a', 3),
 	('d0fabff9-cb76-4dff-9359-9a240418cf58', '807273bb-e792-464f-9ecf-77064e0820a9', '2de39983-9975-4e84-ba34-6296f5cef64a', 2);
+
+SELECT cart_id, user_id
+	FROM public.carts;
+
+SELECT cart_item_id, product_id, quantity, cart_id
+	FROM public.cart_items;
+
+create index index_cart_id on carts(cart_id);
+create index index_cart_item_id on cart_items(cart_item_id);
 
