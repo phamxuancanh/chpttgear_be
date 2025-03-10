ea911b173b4bae510ef31d4cce0d0d8411d858f7
leminhhieu7801 <92864247+leminhhieu7801@users.noreply.github.com>
2025-03-11 00:48:46 +0700

    rating

--- ratingService/src/main/java/com/example/ratingservice/model/Review.java
+++ ratingService/src/main/java/com/example/ratingservice/model/Review.java
@@ -3,27 +3,36 @@ package com.example.ratingservice.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDateTime;
 import java.util.UUID;
+
 @Entity
-@Table(name = "reviews")
+@Table(name = "review_replies")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-public class Review {
+public class Review_Reply {
     @Id
-    @GeneratedValue
     @UuidGenerator(style = UuidGenerator.Style.RANDOM)
-    @Column(name = "category_id")
+    @Column(name = "id", updatable = false, nullable = false)
     private UUID id;
-    @Column(columnDefinition = "VARCHAR(255)")
-    private String userId;
-    @Column(columnDefinition = "VARCHAR(255)")
-    private String productId;
-    @Column
-    private int rating;
-    @Column(columnDefinition = "TEXT")
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id", nullable = false)
+    private Review review;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Column(name = "comment", columnDefinition = "TEXT", nullable = false)
     private String comment;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 }
