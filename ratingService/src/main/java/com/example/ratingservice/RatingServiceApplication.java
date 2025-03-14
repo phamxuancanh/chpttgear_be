1ad709620d452487cc1fcada8e4c475b0858b4f8
leminhhieu7801 <92864247+leminhhieu7801@users.noreply.github.com>
2025-03-14 23:04:56 +0700

    update ratingservice

--- ratingService/src/main/java/com/example/ratingservice/RatingServiceApplication.java
+++ ratingService/src/main/java/com/example/ratingservice/RatingServiceApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RatingServiceApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(RatingServiceApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatingServiceApplication.class, args);
+    }
+
 }
--- ratingService/src/main/java/com/example/ratingservice/repository/IReviewRepository.java
+++ ratingService/src/main/java/com/example/ratingservice/repository/IReviewRepository.java
@@ -1,6 +1,30 @@
-package com.example.ratingservice.repository;
+package com.example.ratingservice.Repository;
+
+import com.example.ratingservice.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.UUID;
 
 @Repository
 public interface IReviewRepository extends JpaRepository<Review, UUID> {
-    Review findReviewById(UUID reviewId);
+    public interface ReviewProjection {
+        UUID getId();
+        int getRating();
+        String getReview();
+        UUID getUserId();
+        UUID getReplyId();
+        UUID getProductId();
+        String getParentId();
+        Instant getCreateDate();
+        Instant getUpdateDate();
+        String getName();
+    }
+    @Query(value = "Select R.*, CONCAT(U.\"firstName\", ' ', U.\"lastName\") AS name  From public.reviews R join public.users U ON R.user_id = U.id WHERE R.product_id = :product_id", nativeQuery = true)
+    List<ReviewProjection> findByProductId(@Param("product_id") UUID product_id);
 }
