ece8ab18382002293696ed234434837055124f89
phamxuancanh <pxcpaze@gmail.com>
2025-03-17 01:39:49 +0700

    update constructor

--- productService/src/main/java/fit/iuh/com/dto/ProductWithCategory_DTO.java
+++ productService/src/main/java/fit/iuh/com/dto/ProductWithCategory_DTO.java
@@ -2,15 +2,9 @@ package fit.iuh.com.dto;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
 @ToString
@@ -27,6 +21,24 @@ public class ProductWithCategory_DTO {
     private int guaranteePeriod;
     private LocalDateTime modifiedDate;
     private UUID category_id;
-    private String category_type;
+    private String categoryName;
 
+    // ✅ Constructor đúng thứ tự với HQL
+    public ProductWithCategory_DTO(UUID id, String name, String description, double price, String image,
+                                   String brand, String color, String size, double weight, int guaranteePeriod,
+                                   LocalDateTime modifiedDate, UUID category_id, String categoryName) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.image = image;
+        this.brand = brand;
+        this.color = color;
+        this.size = size;
+        this.weight = weight;
+        this.guaranteePeriod = guaranteePeriod;
+        this.modifiedDate = modifiedDate;
+        this.category_id = category_id;
+        this.categoryName = categoryName;
+    }
 }
