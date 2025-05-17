a37dd3c24c9c0f2a15b74ec87425a7f612f6ae6b
tranminhtri <53221375+tranmintri@users.noreply.github.com>
2025-05-18 00:06:52 +0700

    revise env

--- orderService/index.js
+++ orderService/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://chpttgear-fe.vercel.app/",
     credentials: true,
   })
 );
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Middleware cho CORS
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://chpttgear-fe.vercel.app/");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "X-Requested-With,content-type, Authorization"
