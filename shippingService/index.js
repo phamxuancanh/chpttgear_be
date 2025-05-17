9ba34093e85e671f529e0458b8533a8d79f7d955
tranminhtri <53221375+tranmintri@users.noreply.github.com>
2025-05-18 00:24:39 +0700

    fix docker file

--- shippingService/index.js
+++ shippingService/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "*",
+    origin: "https://chpttgear-fe.vercel.app/",
   })
 );
 app.use(bodyParser.json());
